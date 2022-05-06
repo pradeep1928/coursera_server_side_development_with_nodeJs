c24c15a86c3b3eaecd1feee379128098fb64adbe
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-05-06 17:49:08 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
 
+const Dishes = require("./models/dishes");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const url  = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+connect.then((db) => {
+  console.log("Successefully connected to mongodb");
+}).catch((error) => console.log(error));
 
 var app = express();
 
@@ -17,12 +24,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// configurations
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes and endpoints 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
