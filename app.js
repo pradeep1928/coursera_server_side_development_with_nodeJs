6b49792417ea130078da9507bfcccdc32afb6e77
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-05-09 14:41:18 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
 
-const Dishes = require("./models/dishes");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -29,7 +28,38 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Authentication 
+const auth = (req, res, next) => {
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    var err = new Error("You are not authenticated");
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+  else {
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+    if (username == 'admin' && password == 'password') {
+      next();
+    } 
+    else {
+      var err = new Error("You are not authenticated");
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+  }
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routes and endpoints 
 app.use('/', indexRouter);
