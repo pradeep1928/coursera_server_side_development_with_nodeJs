e8fd4a447f70f685c67e91c33dea01b6e71b2506
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2022-05-28 20:41:37 +0530

    Assignment-4 adding dishes to favorites

---
+++ models/favorite.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const favouriteSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dishes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Dish'
+        }
+    ],
+
+}, {
+    usePushEach: true
+}, {
+    timestamps: true
+});
+
+var Favourites = mongoose.model('Favourite', favouriteSchema);
+
+module.exports = Favourites;
